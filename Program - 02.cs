1f241525989057509101e12a3327ffb84035114a
Muhammad Shaheer <suburban259@gmail.com>
2023-10-29 23:59:44 +0500

    This is second prorgam.

---
+++ Program - 02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace prime
+{
+    class Pro
+    {
+        public static void Main()
+        {
+            int i, c, m = 1;
+            Console.WriteLine("Enter a Value: ");
+            int n = int.Parse(Console.ReadLine());
+            c = 0; i = 1;
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    if (i != n)
+                        m = i;
+                    c++;
+                }
+                i++;
+            }
+            if (c == 2)
+            {
+                Console.WriteLine("num is prime");
+            }
+            else
+            {
+                Console.WriteLine(m + "is largest factor" + n);
+                Console.ReadLine();
+            }
+        }
+    }
+}
